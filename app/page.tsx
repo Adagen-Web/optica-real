8ada8adb2c091f294e3890b560c43d6be808c416
Mateus Paulart <mateusp23@rede.ulbra.br>
2025-10-17 09:35:50 -0300

    remove social media

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <SectionPublico />
       <SectionSobre />
 
-      <SocialMediaCarousel />
+      {/* <SocialMediaCarousel /> */}
       <SectionExperiencia />
     </>
   );
